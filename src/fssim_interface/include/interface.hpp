1202ebd0bb0521033bcf6f72b3a4abcaa3ae686c
kabzo <kabzanj@student.ethz.ch>
2019-01-08 00:41:59 +0100

    Update

--- src/fssim_interface/include/interface.hpp
+++ src/fssim_interface/include/interface.hpp
@@ -1,13 +1,25 @@
 /*
- * AMZ Driverless Project
- *
+ * AMZ-Driverless
  * Copyright (c) 2018 Authors:
- *   - Juraj Kabzan <kabzanj@ethz.ch>
+ *   - Juraj Kabzan <kabzanj@gmail.com>
  *
- * All rights reserved.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #ifndef FSSIM_INTERFACE_INTERFACE_HPP
@@ -100,7 +112,6 @@ fsd_common_msgs::Map getMap(const fssim_common::Track &track) {
     for (const geometry_msgs::Point &c : track.cones_right) {
         msg.cone_blue.push_back(getConeFromPoint(c, "blue"));
     }
-
     return msg;
 }
 
